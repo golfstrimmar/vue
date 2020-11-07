5c3a094629bdff4386c4d7b41603cc1704803b01
yushinbox@gmail.com <yushinbox@gmail.com>
2020-11-08 01:10:37 +0300

    +

--- my-vue/src/App.vue
+++ my-vue/src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="training">
+   <h1>Math training</h1>
+   <hr>
+   <app-start-screen v-if="state == 'start'"></app-start-screen>
+   <app-question v-else-if="state == 'question'"></app-question>
+   <app-message v-else-if="state == 'message'"></app-message>
+   <app-result-screen v-else-if="state == 'result'"></app-result-screen>
+   
+   <div v-else>Unknown state</div>
   </div>
 </template>
 
@@ -24,13 +16,22 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+     state: "start"
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.training{
+   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin:20px auto;
+  max-width: 700px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ my-vue/src/components/StartScreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="alert alert-secondary">
+    <h3>Are your ready?</h3>
+    <button class="btn btn-success" @click="some">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    some(){
+      alert(1);
+      }
+  }
+}
+</script>
+
+<style scoped>
+  .alert{
+    text-align: center; 
+  }
+  h3,.btn{
+    margin: 20px 0;	
+  }
+</style>
