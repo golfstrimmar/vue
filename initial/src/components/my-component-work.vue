c108c6219c2a889cc768e8545591cf8013a13dd8
yushinbox@gmail.com <yushinbox@gmail.com>
2020-11-10 13:06:47 +0300

    +

--- initial/src/components/my-component-work.vue
+++ initial/src/components/my-component-work.vue
@@ -34,19 +34,95 @@ isActive
 <div :class="[  isActive ? 'active btn'  : 'disabled a', 'default' ]">
 isActive
 </div>
+<hr>
 <!-- ------------------------------- -->
 <h2 :style="{color: 'purple', width: width + 'px', background: '#F08080',margin: '20px auto'		}"> текст</h2>
+<hr>
 <!-- ------------------------------- -->
 <ul>
-  <li >{{list}}</li>
+    <li
+     v-for="item in items" :key="item.message"
+     >
+    {{ item.message }}
+  </li>
 </ul>
+<hr>
 <!-- ------------------------------- -->
 <ul>
-  <li></li>	
+   <li 
+   v-for="(item, index) in items"
+   :key="item.message"
+   >
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
 </ul>
+<hr>
+<!-- ------------вывод списком------------------- -->
+<ul>
+  <li
+  v-for="user in users"
+  :key="user.message"
+  >
+{{user.id}} : {{user.name}}
+  </li>
+</ul>
+<hr>
+<!-- ------------вывод таблицей------------------- -->
+<table border="1">
+  <thread>
+    <th>
+      iD
+    </th>
+    <th>
+      Имя
+    </th>
+  </thread>
+  <tbody>
+    <tr
+     v-for="user in users"
+  :key="user.message"
+     >
+      <td>{{user.id}}</td>
+      <td>{{user.name}}</td>
+    </tr>
+  </tbody>
+</table>
+<hr>
+<!-- -----------methods-------------------- -->
+<!-- -----------methods-------------------- -->
+<!-- -----------methods-------------------- -->
+
+<!-- click -->
+<div >
+  <button 
+  @click="counter += 1"
+  >button</button>
+  <p>Кнопка выше была нажата {{ counter }} раз</p>
+</div>
+<hr>
+<!-- ---------поменяет по клику на кнопку значение свойства id у второго элемента массива users---------------------- -->
+<div >
+  <button 
+  @click="users[1].id=20"
+  >поменяет по клику на кнопку значение свойства id у второго элемента массива users</button>
+</div>
+<hr>
 <!-- ------------------------------- -->
+<div >
+  <button 
+  @click="nameFunction()"
+  >По клику вызовет функцию</button>
+
+</div>
+<hr>
 <!-- ------------------------------- -->
+<hr>
 <!-- ------------------------------- -->
+<hr>
+<!-- ------------------------------- -->
+<hr>
+<!-- ------------------------------- -->
+<hr>
 <!-- ------------------------------- -->
 </div>
 
@@ -63,19 +139,27 @@ data(){
   text:"Helloo!!!",
    id: 10,
    title: "заголовок",
-   status: 0,
+   status: 1,
    isActive: true,
    isBtn: true,
    width: 115,
-   lists: ["один","два","три"],
-// users: [
-  // {id:1,name: "Иван"},
-  // {id:2,name: "Марья"},
-  // {id:3,name: "Филипп"}
-//  ]
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ],
+    parentMessage: 'Родитель',
+    users: [
+      {id:1, name: "Иван" },
+      {id:2, name: "Марья" }
+    ],    counter: 0
+}
+
+},
+methods:{
+nameFunction(){alert('nameFunction')}
 }
 }
-}
+
 </script>
 
 <style >
